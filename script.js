a97f6ef12f6d4a10e0187f541a4f170f180d42c6
Shreyas Shrawage <shreyas.shrawage2000@gmail.com>
2021-07-06 15:29:24 +0530

    added more lables

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let nn;
-let div_0, div_1, div_2, div_3;
+let div_0, div_1, div_2, div_3, div_4;
+let div_5, div_6, div_7, div_8, div_9;
 function setup() {
   canvas = createCanvas(400, 400);
   background(0);
@@ -10,6 +11,12 @@ function setup() {
   div_1 = select(".div_1");
   div_2 = select(".div_2");
   div_3 = select(".div_3");
+  div_4 = select(".div_4");
+  div_5 = select(".div_5");
+  div_6 = select(".div_6");
+  div_7 = select(".div_7");
+  div_8 = select(".div_8");
+  div_9 = select(".div_9");
 
   nn = ml5.neuralNetwork({
     inputs: [56, 56, 4],
@@ -31,7 +38,7 @@ function setup() {
 function draw() {
   if (mouseIsPressed) {
     stroke(255);
-    strokeWeight(8);
+    strokeWeight(40);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
@@ -58,6 +65,18 @@ function classifyCanvas() {
     div_2.removeClass("bg-dark");
     div_3.removeClass("text-white");
     div_3.removeClass("bg-dark");
+    div_4.removeClass("text-white");
+    div_4.removeClass("bg-dark");
+    div_5.removeClass("text-white");
+    div_5.removeClass("bg-dark");
+    div_6.removeClass("text-white");
+    div_6.removeClass("bg-dark");
+    div_7.removeClass("text-white");
+    div_7.removeClass("bg-dark");
+    div_8.removeClass("text-white");
+    div_8.removeClass("bg-dark");
+    div_9.removeClass("text-white");
+    div_9.removeClass("bg-dark");
 
     if (result[0].label == "zero") {
       div_0.addClass("text-white");
@@ -68,18 +87,36 @@ function classifyCanvas() {
     } else if (result[0].label == "two") {
       div_2.addClass("text-white");
       div_2.addClass("bg-dark");
-    } else {
+    } else if (result[0].label == "three") {
       div_3.addClass("text-white");
       div_3.addClass("bg-dark");
+    } else if (result[0].label == "four") {
+      div_4.addClass("text-white");
+      div_4.addClass("bg-dark");
+    } else if (result[0].label == "five") {
+      div_5.addClass("text-white");
+      div_5.addClass("bg-dark");
+    } else if (result[0].label == "six") {
+      div_6.addClass("text-white");
+      div_6.addClass("bg-dark");
+    } else if (result[0].label == "seven") {
+      div_7.addClass("text-white");
+      div_7.addClass("bg-dark");
+    } else if (result[0].label == "eight") {
+      div_8.addClass("text-white");
+      div_8.addClass("bg-dark");
+    } else if (result[0].label == "nine") {
+      div_9.addClass("text-white");
+      div_9.addClass("bg-dark");
     }
 
-    // textP.html(
-    //   `${result[0].label} : ${result[0].confidence * 100} %<br>${
-    //     result[1].label
-    //   } : ${result[1].confidence * 100} %<br>${result[2].label} : ${
-    //     result[2].confidence * 100
-    //   } %<br>${result[3].label} : ${result[3].confidence * 100} %<br>`
-    // );
+    textP.html(
+      `${result[0].label} : ${result[0].confidence * 100} %<br>${
+        result[1].label
+      } : ${result[1].confidence * 100} %<br>${result[2].label} : ${
+        result[2].confidence * 100
+      } %<br>${result[3].label} : ${result[3].confidence * 100} %<br>`
+    );
     classifyCanvas();
   });
 }
--- trainModel.js
+++ trainModel.js
@@ -1,14 +1,26 @@
 let zeros = [],
   ones = [],
   twos = [],
-  threes = [];
+  threes = [],
+  fours = [],
+  fives = [],
+  sixes = [],
+  sevens = [],
+  eights = [],
+  nines = [];
 
 function preload() {
-  for (let i = 0; i <= 100; i++) {
-    zeros[i] = loadImage(`data/zeros/zero (${i}).jpg`);
-    ones[i] = loadImage(`data/ones/one (${i}).jpg`);
-    twos[i] = loadImage(`data/twos/two (${i}).jpg`);
-    threes[i] = loadImage(`data/threes/three (${i}).jpg`);
+  for (let i = 1; i <= 100; i++) {
+    zeros[i - 1] = loadImage(`data/zeros/zero (${i}).jpg`);
+    ones[i - 1] = loadImage(`data/ones/one (${i}).jpg`);
+    twos[i - 1] = loadImage(`data/twos/two (${i}).jpg`);
+    threes[i - 1] = loadImage(`data/threes/three (${i}).jpg`);
+    fours[i - 1] = loadImage(`data/fours/four (${i}).jpg`);
+    fives[i - 1] = loadImage(`data/fives/five (${i}).jpg`);
+    sixes[i - 1] = loadImage(`data/sixes/six (${i}).jpg`);
+    sevens[i - 1] = loadImage(`data/sevens/seven (${i}).jpg`);
+    eights[i - 1] = loadImage(`data/eights/eight (${i}).jpg`);
+    nines[i - 1] = loadImage(`data/nines/nine (${i}).jpg`);
   }
 }
 let canvas;
@@ -21,19 +33,25 @@ function setup() {
     debug: true,
   });
 
-  for (let i = 0; i <= 100; i++) {
+  for (let i = 0; i < 100; i++) {
     nn.addData({ image: zeros[i] }, { target: "zero" });
     nn.addData({ image: ones[i] }, { target: "one" });
     nn.addData({ image: twos[i] }, { target: "two" });
     nn.addData({ image: threes[i] }, { target: "three" });
+    nn.addData({ image: fours[i] }, { target: "four" });
+    nn.addData({ image: fives[i] }, { target: "five" });
+    nn.addData({ image: sixes[i] }, { target: "six" });
+    nn.addData({ image: sevens[i] }, { target: "seven" });
+    nn.addData({ image: eights[i] }, { target: "eight" });
+    nn.addData({ image: nines[i] }, { target: "nine" });
   }
 
   nn.normalizeData();
 
   nn.train(
     {
-      epochs: 32,
-      batchSize: 12,
+      epochs: 70,
+      batchSize: 50,
     },
     finishedTraining
   );
